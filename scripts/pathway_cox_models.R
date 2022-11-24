7b9caa8eb233882220fa3bb5b798554829752171
rmall <administrator@rmall.stjude.sjcrh.local>
2022-11-24 15:27:39 -0600

    Revised pathway analysis scripts and added tertile based results

--- scripts/pathway_cox_models.R
+++ scripts/pathway_cox_models.R
@@ -71,6 +71,10 @@ test_pathway_activities <- gsva(as.matrix(final_test_all_expr_df), gset.idx.list
 #test_cyt_score <- c(new_gse140082_out[[2]], new_gse32062_out[[2]], new_gse53963_out[[2]])
 train_cyt_score <- as.numeric(as.vector(colMeans(final_all_expr_df[c("GZMA","PRF1"),])))
 test_cyt_score <- as.numeric(as.vector(colMeans(final_test_all_expr_df[c("GZMA","PRF1"),])))
+train_constru_score <- c(new_gse82191_out[[9]],new_gse9891_out[[9]], new_gseov3_out[[9]])
+test_constru_score <- c(new_gse140082_out[[9]],new_gse32062_out[[9]], new_gse53963_out[[9]])
+train_cyt_tertiles <- c(new_gse82191_out[[2]],new_gse9891_out[[2]], new_gseov3_out[[2]])
+test_cyt_tertiles <- c(new_gse140082_out[[2]],new_gse32062_out[[2]], new_gse53963_out[[2]])
 train_constru_tertiles <- c(new_gse82191_out[[3]], new_gse9891_out[[3]], new_gseov3_out[[3]])
 test_constru_tertiles <- c(new_gse140082_out[[3]], new_gse32062_out[[3]], new_gse53963_out[[3]])
 train_os_event <- c(new_gse82191_out[[4]], new_gse9891_out[[4]], new_gseov3_out[[4]])
@@ -79,11 +83,11 @@ test_os_event <- c(new_gse140082_out[[4]], new_gse32062_out[[4]], new_gse53963_o
 test_os_time <- c(new_gse140082_out[[5]], new_gse32062_out[[5]], new_gse53963_out[[5]])
 
 #Make the training and testing data frame
-train_df <- as.data.frame(cbind(data.frame(time=train_os_time, status=train_os_event, cyt_score = train_cyt_score, constru_tertiles = train_constru_tertiles), t(train_pathway_activities)))
-test_df <- as.data.frame(cbind(data.frame(time=test_os_time, status=test_os_event, cyt_score = test_cyt_score, constru_tertiles = test_constru_tertiles), t(test_pathway_activities)))
+train_df <- as.data.frame(cbind(data.frame(time=train_os_time, status=train_os_event, cyt_score = train_cyt_score, constru_score = train_constru_score, cyt_tertiles = train_cyt_tertiles, constru_tertiles = train_constru_tertiles), t(train_pathway_activities)))
+test_df <- as.data.frame(cbind(data.frame(time=test_os_time, status=test_os_event, cyt_score = test_cyt_score, constru_score = test_constru_score, cyt_tertiles = test_cyt_tertiles, constru_tertiles = test_constru_tertiles), t(test_pathway_activities)))
 save(train_df, test_df,file="Data/Training_Testing_OS.Rdata")
 
-pathway_names <- colnames(train_df)[c(5:ncol(train_df))]
+pathway_names <- colnames(train_df)[c(7:ncol(train_df))]
 train_cox_proportional_df <- NULL
 for (k in 1:length(pathway_names))
 {
@@ -98,7 +102,7 @@ for (k in 1:length(pathway_names))
   {
     sample_ids <- id_list[[i]]
     sample_df <- train_df[sample_ids,c(1:4)]
-    sample_cox <- coxph(Surv(time,status)~cyt_score, data = sample_df)
+    sample_cox <- coxph(Surv(time,status)~constru_score, data = sample_df)
     temp_cox <- get_cox_info(sample_cox)
     temp_cox <- c(pathway_names[k],names(id_list)[i],temp_cox)
     train_cox_proportional_df <- rbind(train_cox_proportional_df, temp_cox)
@@ -114,7 +118,7 @@ train_cox_proportional_df$upper <- as.numeric(as.vector(train_cox_proportional_d
 train_cox_proportional_df$wald.test <- as.numeric(as.vector(train_cox_proportional_df$wald.test))
 train_cox_proportional_df$p.value <- as.numeric(as.vector(train_cox_proportional_df$p.value))
 train_cox_proportional_df$p.adjust <- signif(p.adjust(train_cox_proportional_df$p.value, method="fdr"),digits=2)
-write.table(x=train_cox_proportional_df,file="results/Training_Cox_Proportional_Pathway_Cytscore.csv",quote = F, row.names=F, col.names=T, sep="\t")
+write.table(x=train_cox_proportional_df,file="results/Training_Cox_Proportional_Pathway_Constru_score.csv",quote = F, row.names=F, col.names=T, sep="\t")
 
 ################################################################################
 test_cox_proportional_df <- NULL
@@ -131,7 +135,7 @@ for (k in 1:length(pathway_names))
   {
     sample_ids <- id_list[[i]]
     sample_df <- test_df[sample_ids,c(1:4)]
-    sample_cox <- coxph(Surv(time,status)~cyt_score, data = sample_df)
+    sample_cox <- coxph(Surv(time,status)~constru_score, data = sample_df)
     temp_cox <- get_cox_info(sample_cox)
     temp_cox <- c(pathway_names[k],names(id_list)[i],temp_cox)
     test_cox_proportional_df <- rbind(test_cox_proportional_df, temp_cox)
@@ -147,7 +151,7 @@ test_cox_proportional_df$upper <- as.numeric(as.vector(test_cox_proportional_df$
 test_cox_proportional_df$wald.test <- as.numeric(as.vector(test_cox_proportional_df$wald.test))
 test_cox_proportional_df$p.value <- as.numeric(as.vector(test_cox_proportional_df$p.value))
 test_cox_proportional_df$p.adjust <- signif(p.adjust(test_cox_proportional_df$p.value, method="fdr"),digits=2)
-write.table(x=test_cox_proportional_df,file="results/Testing_Cox_Proportional_Pathway_Cytscore.csv",quote = F, row.names=F, col.names=T, sep="\t")
+write.table(x=test_cox_proportional_df,file="results/Testing_Cox_Proportional_Pathway_Constru_score.csv",quote = F, row.names=F, col.names=T, sep="\t")
 
 #Make the plot based on pathway tertiles and hazard ratio comparison for CYT score in each tertile
 ################################################################################
@@ -183,7 +187,7 @@ ha = HeatmapAnnotation(
   show_legend=F)
 col_fun1 <- colorRamp2(c(0.5,1,1.5),c("blue","white","red"))
 ht_pathway_train = Heatmap(matrix=train_pathway_hazards_matrix, col_fun1, 
-                       name = "Hazards Ratio", column_title = qq("Hazards for CYT score across Pathway Tertiles in Training Set"),
+                       name = "Hazards Ratio", column_title = qq("Hazards for Constru score across Pathway Tertiles in Training Set"),
                        cell_fun = function(j, i, x, y, width, height, fill) {
                          grid.rect(x = x, y = y, width = width, height = height, 
                                    gp = gpar(col = "grey", fill = NA))
@@ -225,13 +229,13 @@ ht_pathway_train = Heatmap(matrix=train_pathway_hazards_matrix, col_fun1,
                        border = T,
                        heatmap_legend_param = list(direction = "horizontal")
 )
-pdf("results/Training_Cox_Proportional_Pathway_Tertiles_CYT_Score.pdf",height = 10, width=6)
+pdf("results/Training_Cox_Proportional_Pathway_Tertiles_Constru_Score.pdf",height = 10, width=6)
 draw(ht_pathway_train, heatmap_legend_side="bottom")
 dev.off()
 
 #Make the figure for the test set
 ht_pathway_test = Heatmap(matrix=test_pathway_hazards_matrix, col_fun1, 
-                      name = "Hazards Ratio", column_title = qq("Hazards for CYT score across Pathway Tertiles in Test Set"),
+                      name = "Hazards Ratio", column_title = qq("Hazards for Constru score across Pathway Tertiles in Test Set"),
                       cell_fun = function(j, i, x, y, width, height, fill) {
                         grid.rect(x = x, y = y, width = width, height = height, 
                                   gp = gpar(col = "grey", fill = NA))
@@ -273,7 +277,7 @@ ht_pathway_test = Heatmap(matrix=test_pathway_hazards_matrix, col_fun1,
                       border = T,
                       heatmap_legend_param = list(direction = "horizontal")
 )
-pdf("results/Test_Cox_Proportional_Pathway_Tertiles_CYT_Score.pdf",height = 10, width=6)
+pdf("results/Test_Cox_Proportional_Pathway_Tertiles_Constru_Score.pdf",height = 10, width=6)
 draw(ht_pathway_test, heatmap_legend_side="bottom")
 dev.off()
 
